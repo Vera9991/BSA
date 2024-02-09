93880add0c5a6e4a610fdac98aa0826391f32fef
Vera9991 <ver5327@yandex.ru>
2024-02-09 09:57:12 +0300

    задание

---
+++ HW.py
@@ -0,0 +1,6 @@
+print("Я как (кто,), хочу (что?), чтобы (что?)")
+any_string = input("введите фразу") #ввод строки#
+rev_string = ''.join(reversed(any_string.split())) #реверс фразы#
+print ("Введенная фраза:", any_string)
+print("Реверс фразы:", rev_string)
+#вывод строк#
