3f7cd42715ca7c05755ccda069f44186ffdaf21a
Vera9991 <ver5327@yandex.ru>
2024-02-19 00:19:21 +0300

    билет

--- happyticket.py
+++ happyticket.py
@@ -1,10 +1,25 @@
-def is_happy_ticket(ticket_number):
-    half_length = len(ticket_number) // 2
-    first_half_sum = sum(map(int, ticket_number[:half_length]))
-    second_half_sum = sum(map(int, ticket_number[half_length:]))
+def is_happy_ticket(s):
+    d = len(s)
 
-    return first_half_sum == second_half_sum
+    # Проверяем, делится ли число d на 2 без остатка#
+    if d % 2 == 0:
+        middle = d // 2  # Узнаем середину строки
+        sum_first_half = 0
+        sum_second_half = 0
+        # Перебираем символы первой половины строки#
+        for j in range(middle):
+            sum_first_half += int(s[j])
 
-# Примеры использования:
-ticket_number = input("Введите шестизначный номер: ")
-print(is_happy_ticket(ticket_number))
+        # Перебираем символы второй половины строки#
+        for i in range(middle, d):
+            sum_second_half += int(s[i])
+
+        # Проверяем равенство сумм первой и второй половин#
+        return sum_first_half == sum_second_half
+    else:
+        return False  # Если число символов нечетное, возвращаем False#
+
+# Пример использования
+input_str = input("Введите 6-ти значное число: ")
+result = is_happy_ticket(input_str)
+print(result)
